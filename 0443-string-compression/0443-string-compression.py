493aaeb4c832d049ae592c2645410da800b2edd0
Sai Wanna Aung <45900997+SaiWannaAung1@users.noreply.github.com>
2025-02-14 20:09:24 +0000

    Time: 0 ms (100%), Space: 17.8 MB (73.19%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -4,23 +4,22 @@ class Solution:
         :type chars: List[str]
         :rtype: int
         """
-        write_index = 0  # Index to overwrite in the chars array
-        read_index = 0   # Index to read from the chars array
+        write_index = 0  
+        read_index = 0   
         
         while read_index < len(chars):
-            char = chars[read_index]  # Get the current character
+            char = chars[read_index] 
             count = 0
             
-            # Count how many times the current character repeats consecutively
+        
             while read_index < len(chars) and chars[read_index] == char:
                 read_index += 1
                 count += 1
                 
-            # Write the character
+    
             chars[write_index] = char
             write_index += 1
             
-            # If the character repeats more than once, write the count
             if count > 1:
                 for digit in str(count):
                     chars[write_index] = digit
